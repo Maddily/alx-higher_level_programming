4c678c8e332859d96b97869817d80ec9bff5c881
Maddie <115709272+Maddily@users.noreply.github.com>
2023-10-31 20:18:49 +0200

    Print the alphabet in reverse alternating cases

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Display the alphabet in reverse, alternating between lower and uppercase"""
+
+
+i = 122
+while i >= 65:
+    print(chr(i), end="")
+    if i == 65:
+        break
+    if i >= 97 and i <= 122:
+        i -= 33
+    else:
+        i += 31
