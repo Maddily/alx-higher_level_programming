0d7f092f53238b96906869e5b9b3cb0c8c3f143f
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-02 13:48:53 +0200

    Use a dict

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,30 +1,23 @@
 #!/usr/bin/python3
-"""Import functions from a module and handle basic operations"""
+"""Import functions from a module and call them on arguments"""
 
 
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
 
-    operators = ["+", "-", "*", "/"]
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
 
     if len(argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    if argv[2] not in operators:
+    if argv[2] not in list(operators.keys()):
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
 
     a = int(argv[1])
     b = int(argv[3])
 
-    match argv[2]:
-        case "+":
-            print("{} + {} = {}".format(a, b, add(a, b)))
-        case "-":
-            print("{} - {} = {}".format(a, b, sub(a, b)))
-        case "*":
-            print("{} * {} = {}".format(a, b, mul(a, b)))
-        case "/":
-            print("{} / {} = {}".format(a, b, div(a, b)))
+    print("{} {} {} = {}".format(a, argv[2], b, operators[argv[2]](a, b)))
+
