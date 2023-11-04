f3a96238e8d2f961b3f38b906d484c8e9c064806
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-04 08:51:20 +0200

    Reverse only if list is true

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,6 +5,6 @@
 def print_reversed_list_integer(my_list=[]):
     my_list.sort(reverse=True)
 
-    if len(my_list) > 1:
+    if my_list is True:
         for int in my_list:
             print("{:d}".format(int))
