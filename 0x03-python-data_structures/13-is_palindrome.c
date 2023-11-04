4495105133f44ecfa45cea9cfd5fa5db0a9107b9
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-04 13:13:27 +0200

    Use a more efficient way

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,28 @@
 #include "lists.h"
 
 /**
+ * reverse_linked_list - Reverses a linked list
+ * @head: A pointer to the first node in the list
+ *
+ * Return: A pointer to the first node in the reversed list
+ */
+listint_t *reverse_linked_list(listint_t *head)
+{
+	listint_t *temp1, *temp2;
+
+	temp2 = NULL;
+	while (head != NULL)
+	{
+		temp1 = head->next;
+		head->next = temp2;
+		temp2 = head;
+		head = temp1;
+	}
+	head = temp2;
+
+	return (head);
+}
+/**
  * is_palindrome - Checks if a singly linked list is a palindrome
  *
  * @head: A pointer to the pointer pointing to the first node in a list
@@ -9,40 +31,44 @@
  */
 int is_palindrome(listint_t **head)
 {
-	int length = 0, i = 0, j, *values = NULL;
-	listint_t *last, *current;
+	listint_t *second_part, *middle = NULL, *tortoise, *hare, *temp1, *temp2;
+	int palindrome = 1;
 
 	if (*head == NULL || (*head)->next == NULL)
 		return (1);
 
-	last = current = *head;
+	tortoise = hare = *head;
 
-	while (last != NULL)
+	while (hare != NULL && hare->next != NULL)
 	{
-		last = last->next;
-		length++;
+		hare = hare->next->next;
+		tortoise = tortoise->next;
 	}
-
-	values = malloc(length * sizeof(int));
-	if (values == NULL)
-		return (0);
-
-	values[i++] = (*head)->n;
-
-	while (current->next != NULL)
+	if (hare != NULL)
 	{
-		current = current->next;
-		values[i++] = current->n;
+		middle = tortoise;
+		tortoise = tortoise->next;
 	}
-	for (i = 0, j = length - 1; i < j; i++, j--)
+	second_part = reverse_linked_list(tortoise);
+
+	temp1 = *head;
+	temp2 = second_part;
+	while (temp2 != NULL)
 	{
-		if (values[i] != values[j])
+		if (temp1->n != temp2->n)
 		{
-			free(values);
-			return (0);
+			palindrome = 0;
+			break;
 		}
+		temp1 = temp1->next;
+		temp2 = temp2->next;
 	}
+	second_part = reverse_linked_list(second_part);
 
-	free(values);
-	return (1);
+	if (middle != NULL)
+		middle->next = second_part;
+	else
+		*head = second_part;
+
+	return palindrome;
 }
