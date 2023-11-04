bbd400deb20bb6b5a14a711f113d9ad43cb3954b
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-04 08:57:24 +0200

    Use a different sorting method

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,9 +3,7 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.sort(reverse=True)
 
     if my_list:
-        # for int in reversed(my_list):
-        for int in my_list:
+        for int in reversed(my_list):
             print("{:d}".format(int))
