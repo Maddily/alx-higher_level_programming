0df7b567c7bb865a4cb26938caee9433ca65ac1c
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-08 13:15:41 +0200

    Use a different method

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,7 @@
 def print_sorted_dictionary(a_dictionary):
     """Print a dictionary by ordered keys"""
 
-    for key in sorted(a_dictionary):
-        print("{}: {}".format(key, a_dictionary[key]))
+    new_dict = {key: a_dictionary[key] for key in sorted(a_dictionary)}
+
+    for k, v in new_dict.items():
+        print("{}: {}".format(k, v))
