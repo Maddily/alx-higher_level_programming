1aef8b927b7e1c6f70c66a884d1e3a41ed78d31b
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-21 20:58:02 +0200

    Add docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
+"""Define a Python class called MagicClass"""
+
 import math
+"""The 'math' module provides mathematical functions and constants"""
 
 
 class MagicClass:
+    """A class representing a magical circle."""
+
     def __init__(self, radius=0):
+        """Initialize a new MagicClass instance"""
+
         self.__radius = 0
 
         if type(radius) is not int and type(radius) is not float:
@@ -12,7 +19,11 @@ class MagicClass:
         self.__radius = radius
 
     def area(self):
+        """Calculate the area of the magical circle"""
+
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
+        """Calculate the circumference of the magical circle"""
+
         return 2 * math.pi * self.__radius
