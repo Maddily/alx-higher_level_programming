13fddadf8242ff761ce0b9e294aa1b1ce4b74b6d
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-21 15:12:41 +0200

    Build off 3-square.py to get and set

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Define a Square type of object"""
+
+
+class Square:
+    """Defines a private attribute"""
+
+    def __init__(self, size=0):
+        """Initializes <size> private attribute to what would be passed
+        as an argument for <size>"""
+
+        self.__size = size
+
+    def is_int(self):
+        """Checks if a value is an integer"""
+
+        if not isinstance(self.__size, int):
+            raise TypeError("size must be an integer")
+        elif self.__size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        """Returns the area of the square"""
+
+        self.is_int()
+
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Retrieve the size"""
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size"""
+
+        self.__size = value
