637c57db77584c7d96167ba31a28109042f48b4b
Maddie <115709272+Maddily@users.noreply.github.com>
2023-11-28 15:14:15 +0200

    Use slots

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -11,17 +11,4 @@ class LockedClass:
     except if the new instance attribute is called first_name.
     """
 
-    def __setattr__(self, name, value):
-        """
-        Overrides the __setattr__ method
-
-        Parameters:
-        - name: The name of an attribute
-        - value: The value of the attribute with the name `name`
-        """
-
-        if name != "first_name":
-            raise AttributeError(
-                    f"'LockedClass' object has no attribute '{name}'"
-                )
-        super().__setattr__(name, value)
+    __slots__ = ["first_name"]
