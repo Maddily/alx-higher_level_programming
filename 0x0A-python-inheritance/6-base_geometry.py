4160821a576ed3a68463176636fa54c7eef7d031
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-04 12:29:22 +0200

    Build off 5-base_geometry and write a function
    The function does nothing except for raising an exception.

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module Name: 5-base_geometry
+
+Description: This module defines an empty class `BaseGeometry`.
+"""
+
+
+class BaseGeometry():
+    """
+    Defines a function `area`
+
+    Public Instance Methods:
+    - area(): Raises an exception
+    """
+
+    def area(self):
+        """Raises an exception"""
+
+        raise Exception("area() is not implemented")
