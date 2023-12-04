e293458b185eebaf3e85ae83110ef95cb2abc7b5
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-04 14:28:13 +0200

    Build off 10-square and update str()

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Module Name: 11-square
+
+Description: This module defines a class `Square`
+"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Defines a square
+
+    Attributes:
+    - size: A private instance attribute for the size of a square
+    """
+
+    def __init__(self, size):
+        """Initializes"""
+
+        self.__size = size
+        Rectangle.integer_validator(self, "size", self.__size)
+
+    def area(self):
+        """Returns the area of the square"""
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """Returns a string representation of a rectangle object"""
+
+        return f"[Square] {self.__size}/{self.__size}"
