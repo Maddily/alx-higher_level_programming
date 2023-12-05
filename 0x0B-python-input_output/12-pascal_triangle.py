5710dd8a5016ecb3b12900cf88cd45f8a38116cc
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-05 19:40:45 +0200

    Implement pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Module Name: 12-pascal_triangle
+
+Description: This module defines a function `pascal_triangle`.
+Check the function's documentation for details.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers
+    representing the Pascal's triangle of n
+    """
+
+    triangle = [[1], [1, 1]]
+    sub_triangle = []
+    i = 2
+    j = 0
+
+    while i < 5:
+        while j < i:
+            if j == 0:
+                sub_triangle.append(1)
+                j += 1
+            else:
+                sub_triangle.append(triangle[i - 1][j - 1]
+                                    + triangle[i - 1][j])
+                j += 1
+        sub_triangle.append(1)
+        j = 0
+        triangle.append(sub_triangle)
+        sub_triangle = []
+        i += 1
+
+    return triangle
