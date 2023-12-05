d3e21dee5c856a72d284c1480bf756dd0975f372
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-05 11:51:38 +0200

    Write a function that reads and prints a text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module Name: 0-read_file
+
+Description: This module defines a function `read_file`.
+Check the function's documentation for details.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file and prints it to stdout.
+
+    Parameters:
+    - filename: The name/path of a file
+    """
+
+    with open(filename, encoding="utf-8") as file:
+        print(file.read())
