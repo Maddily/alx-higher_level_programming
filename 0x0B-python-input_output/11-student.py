ced8b3f0552130a7e689ad2a9c1527775c781e74
Maddie <115709272+Maddily@users.noreply.github.com>
2023-12-05 17:07:01 +0200

    Build off 10-student and define a function
    The function replaces existing attributes with new values.

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+"""
+Module Name: 11-student
+
+Description: This module defines a class `Student`.
+Check the class's documentation for details.
+"""
+
+
+class Student:
+    """
+    Defines a student
+
+    Attributes:
+    - first_name: A student's first name
+    - last_name: A student's last name
+    - age: A student's age
+
+    Methods:
+    - to_json() -> dict: Retrieves a dictionary representation of
+    a Student instance
+    - reload_from_json(): Replaces all attributes of an instance
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes"""
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+         Retrieves a dictionary representation of
+         a Student instance
+
+         Parameters:
+         - attrs: Could be a list of attribute names
+        """
+
+        if isinstance(attrs, list) and all(isinstance(attr, str)
+                                           for attr in attrs):
+            return {attr: getattr(self, attr)
+                    for attr in attrs if hasattr(self, attr)
+                    and not callable(getattr(self, attr))}
+
+        return {attr: getattr(self, attr) for attr in self.__dict__
+                if not callable(getattr(self, attr))}
+
+    def reload_from_json(self, json):
+        """
+        Replaces all attributes of an instance
+
+        Parameters:
+        - json: A dictionary
+        """
+
+        for attr, value in json.items():
+            setattr(self, attr, value)
