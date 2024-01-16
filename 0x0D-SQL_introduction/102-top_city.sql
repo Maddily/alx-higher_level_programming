36ba9dab0c8835eaf8e4f4564fc3ed590572ec27
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-16 19:33:47 +0200

    Use month as int

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -2,7 +2,7 @@
 -- and August ordered by temperature (descending).
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
-WHERE month IN ("July", "August")
+WHERE month IN (7, 8)
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
