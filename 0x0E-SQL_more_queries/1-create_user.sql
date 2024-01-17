5bfccea27838de013c8458a9488a7dcd77178b3c
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-17 15:13:39 +0200

    Create a user and give it all privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,7 @@
+-- Creates the MySQL server user user_0d_1.
+-- The user is given all privileges.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+   ON *.*
+   TO 'user_0d_1'@'localhost';
