bcf14a4bca75fd9ceabb03ca1f8b852075d3121f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-01-17 15:44:52 +0200

    Create a database and a table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+/* Creates the database hbtn_0d_usa and the table states on
+the created database */
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id      INT             NOT NULL   UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name    VARCHAR(256)    NOT NULL,
+);
