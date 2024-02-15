090033ec19f0a5bb9d7c1dc5226b3df5cdc78ec9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-15 11:10:40 +0200

    Display states where name matches given argument

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a name starting with N.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state = sys.argv[4]
+
+    db = MySQLdb.connect(user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY states.id",
+        (state,)
+        )
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
