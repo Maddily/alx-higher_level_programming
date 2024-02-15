533672b654ccce63e505ecbcb771ede18b4a7eba
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-15 17:37:53 +0200

    Close session

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -27,3 +27,4 @@ if __name__ == '__main__':
     session.commit()
 
     print(louisiana.id)
+    session.close()
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -27,3 +27,5 @@ if __name__ == '__main__':
         print(f'{first_state.id}: {first_state.name}')
     else:
         print('Nothing')
+
+    session.close()
