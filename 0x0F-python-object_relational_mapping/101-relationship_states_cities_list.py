794a02ee2335dc7122cde7b3c3e0cac2d7dd02f9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-16 14:58:36 +0200

    Update creating engine

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -5,7 +5,7 @@ This script lists all State objects, and corresponding City objects.
 
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, joinedload
+from sqlalchemy.orm import sessionmaker
 from relationship_state import Base, State
 from relationship_city import City
 
@@ -16,7 +16,8 @@ if __name__ == '__main__':
     database = sys.argv[3]
 
     engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost/{database}'
+        f'mysql+mysqldb://{username}:{password}@localhost/{database}',
+        pool_pre_ping=True
         )
 
     Base.metadata.create_all(engine)
