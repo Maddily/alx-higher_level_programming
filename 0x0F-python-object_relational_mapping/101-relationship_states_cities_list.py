bb533348279e5d9e68997e5ddd22d3efb4971fd4
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-16 11:58:03 +0200

    Unuse options

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -22,8 +22,7 @@ if __name__ == '__main__':
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states = session.query(State).options(
-        State.cities).order_by(State.id).all()
+    states = session.query(State).order_by(State.id).all()
 
     for state in states:
         print(f'{state.id}: {state.name}')
