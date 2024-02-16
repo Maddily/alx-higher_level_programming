f58541cf565a2ce34486474b2d1fcff9e972a017
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-16 09:32:15 +0200

    Set a relationship between City and State

---
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+This module contains a class State that links to states table.
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class Attributes:
+    - id (int): A column of auto-generated, unique integer
+    that can't be null and is a primary key.
+    - name (str): A column of a string with maximum
+    128 characters and can't be null.
+    """
+
+    __tablename__ = 'states'
+
+    id = Column(
+        'id', Integer, nullable=False, autoincrement=True, primary_key=True
+        )
+    name = Column('name', String(128), nullable=False)
+
+    cities = relationship('City', cascade='all, delete-orphan', backref='state')
+
+    def __init__(self, name):
+        """Instantiates a state"""
+
+        self.name = name
