2be8da401fca1cea695d216f05bdb0931bd72bd9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-02-16 14:12:42 +0200

    Recreate deleted files

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module contains a class City that links to cities table.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
+
+
+class City(Base):
+    """
+    Class Attributes:
+    - id (int): A column of auto-generated, unique integer
+    that can't be null and is a primary key.
+    - name (str): A column of a string with maximum
+    128 characters and can't be null.
+    - state_id (int): A column representing a foreign key
+    that references the id column of the states table.
+    """
+
+    __tablename__ = 'cities'
+
+    id = Column(
+        'id', Integer, nullable=False, autoincrement=True, primary_key=True
+        )
+    name = Column('name', String(128), nullable=False)
+    state_id = Column(
+        'state_id', Integer, ForeignKey('states.id'), nullable=False
+        )
+
+    def __init__(self, name, state_id):
+        """Instantiates a city"""
+
+        self.name = name
+        self.state_id = state_id
