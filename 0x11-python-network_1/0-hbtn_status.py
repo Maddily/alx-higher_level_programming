00814c8cb0cd87d1f02475abf1200c1b68d2ff61
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-01 11:54:06 +0200

    Update indentation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     with urlopen('https://alx-intranet.hbtn.io/status') as response:
         data = response.read()
 
-    print('Body response:')
-    print('    - type:', type(data))
-    print('    - content:', data)
-    print('    - utf8 content:', data.decode('utf-8'))
+        print('Body response:')
+        print('    - type:', type(data))
+        print('    - content:', data)
+        print('    - utf8 content:', data.decode('utf-8'))
