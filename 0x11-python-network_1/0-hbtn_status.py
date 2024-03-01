027eb7c4703655c750a4fac342e5ece69189cde9
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-01 08:27:59 +0200

    Fetch a url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" This script fetches https://alx-intranet.hbtn.io/status """
+
+from urllib.request import urlopen
+
+
+if __name__ == "__main__":
+    with urlopen('https://alx-intranet.hbtn.io/status') as response:
+        data = response.read()
+
+    print('Body response:')
+    print('    - type:', type(data))
+    print('    - content:', data)
+    print('    - utf8 content:', data.decode('utf-8'))
