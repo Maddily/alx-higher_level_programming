6a7513e5488f9e9400654895a99a8eb93053c04f
Maddie <115709272+Maddily@users.noreply.github.com>
2024-03-26 13:03:38 +0200

    Update the display text

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,5 +6,5 @@ const request = require('request');
 // Display the status code of a GET request.
 request(url, (error, response, body) => {
   if (error) console.error(error);
-  console.log(response.statusCode);
+  console.log('code:', response.statusCode);
 });
